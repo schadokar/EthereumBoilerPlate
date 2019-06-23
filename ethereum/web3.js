df4bcd14945e4af40e161e3affbf2536a554d1e8
Shubham Chadokar <shubhamchadokar04@gmail.com>
2019-06-23 21:23:18 +0530

    master: deploy bug fixed

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -5,13 +5,15 @@ require("dotenv").config();
 let web3;
 let web3Network;
 console.log("NODE_ENV", process.env.NODE_ENV);
-if (process.env.NODE_ENV == "GANACHE" || process.env.NODE_ENV == undefined) {
+if (process.env.NODE_ENV == "GANACHE" || process.env.NODE_ENV === undefined) {
   // web3 = new Web3(ganache.provider());
 
   // local ganache-cli setup
   const eventProvider = new Web3.providers.WebsocketProvider(
     "ws://0.0.0.0:8545"
   );
+  web3 = new Web3(new Web3.providers.HttpProvider("http://0.0.0.0:8545"));
+
   web3Network = "ganache";
   web3.setProvider(eventProvider);
 } else {
--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        limit:"50mb",
-        extended:false,
-        parameterLimit:50000    
-    })
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: false,
+    parameterLimit: 50000
+  })
 );
 
 // use the routes specified in route folder
 app.use("/contract", contractAPIRoutes);
 
-app.use(function(err, req,res, next){
-    res.status(422).send({error: err.message});
+app.use(function(err, req, res, next) {
+  res.status(422).send({ error: err.message });
 });
 
 //listen to the server
-app.listen( port, function(){
-    console.log(`istening to the port ${port} .....`);
+app.listen(port, function() {
+  console.log(`Listening to the port ${port} .....`);
 });
